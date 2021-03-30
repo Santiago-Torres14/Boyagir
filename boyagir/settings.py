a1b13c15065fa4dea89114856da4dfdcbbdd8cc7
Edicson <edicsonpin@gmail.com>
2021-03-30 18:26:42 -0500

    hello5

--- boyagir/settings.py
+++ boyagir/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-#heroku builds:cancel
+#hh
 #heroku addons:create heroku-postgresql:hobby-dev
 #heroku pg:reset DATABASE_URL --confirm boyagir 
 #heroku create myapp --buildpack heroku/python    
